baf4fac53db1f5869f80adf9a31d5f5ae7c86ccc
Fabien Potencier <fabien.potencier@gmail.com>
2011-03-22 07:38:36 +0100

    replaced symfony submodule with the 6 components we need

--- autoload.php
+++ autoload.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once __DIR__.'/vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony' => __DIR__.'/vendor/symfony/src',
+    'Symfony' => __DIR__.'/vendor',
     'Silex'   => __DIR__.'/src',
 ));
 $loader->register();
