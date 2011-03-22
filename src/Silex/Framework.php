e8153254b4c0c8a56ec8ecb0adf073ea0f7eac58
Fabien Potencier <fabien.potencier@gmail.com>
2011-03-22 07:55:17 +0100

    updated email address

--- src/Silex/Framework.php
+++ src/Silex/Framework.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Silex framework.
  *
- * (c) Fabien Potencier <fabien.potencier@symfony.com>
+ * (c) Fabien Potencier <fabien@symfony.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -27,7 +27,7 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 /**
  * The Silex framework class.
  *
- * @author Fabien Potencier <fabien.potencier@symfony.com>
+ * @author Fabien Potencier <fabien@symfony.com>
  */
 class Framework extends HttpKernel implements EventSubscriberInterface
 {
