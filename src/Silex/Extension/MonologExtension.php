8022d089dfcfe5d957594055d78c0990f258a7f3
Igor Wiedler <igor@wiedler.ch>
2011-03-27 17:04:01 +0200

    MonologExtension for logging of requests and errors
    Sample usage:
    
    $app = new Application();
    
    $app->register(new MonologExtension(), array(
    	'monolog.class_path' => __DIR__.'/vendor/monolog/src',
    	'monolog.logfile' => __DIR__.'/application.log',
    ));
    
    $app->get('/hello', function() use ($app) {
    	$app['monolog']->addDebug('currently at hello');
    	return 'Hello World!';
    });
    
    $app->get('/error', function() {
    	throw new RuntimeException('Some error');
    });
    
    $app->run();

---
+++ src/Silex/Extension/MonologExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Silex framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Silex\Extension;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+use Silex\Application;
+use Silex\ExtensionInterface;
+
+use Symfony\Component\HttpKernel\Exception\BaseHttpException;
+
+class MonologExtension implements ExtensionInterface
+{
+    public function register(Application $app)
+    {
+        $app['monolog'] = $app->share(function () use ($app) {
+            $log = new Logger('myapp');
+
+            $app['monolog.configure']($log);
+
+            return $log;
+        });
+
+        $app['monolog.configure'] = $app->protect(function ($log) use ($app) {
+            $log->pushHandler(new StreamHandler($app['monolog.logfile'], Logger::DEBUG));
+        });
+
+        if (isset($app['monolog.class_path'])) {
+            $app['autoloader']->registerNamespace('Monolog', $app['monolog.class_path']);
+        }
+
+        $app->before(function() use ($app) {
+            $app['monolog']->addInfo($app['request']->getMethod() . ' ' . $app['request']->getRequestUri());
+        });
+
+        $app->error(function(\Exception $e) use ($app) {
+            if ($e instanceof BaseHttpException) {
+                $app['monolog']->addWarning($e->getStatusCode() . ' ' . $app['request']->getMethod() . ' ' . $app['request']->getRequestUri());
+            } else {
+                $app['monolog']->addError($e->getMessage());
+            }
+        });
+    }
+}
