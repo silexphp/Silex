c642d13b85eabaf7b91f76fd2f0fc8c49624120b
Igor Wiedler <igor@wiedler.ch>
2011-03-29 10:53:33 +0200

    [TwigExtension] initial test case

---
+++ tests/Silex/Tests/Extension/TwigExtensionTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Silex framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Silex\Tests;
+
+use Silex\Application;
+use Silex\Extension\TwigExtension;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * TwigExtension test cases.
+ *
+ * @author Igor Wiedler <igor@wiedler.ch>
+ */
+class TwigExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        if (!is_dir(__DIR__.'/../../../../vendor/twig')) {
+            $this->markTestSkipped('Twig submodule was not installed.');
+        }
+    }
+
+    public function testRegisterAndRender()
+    {
+        $app = new Application();
+
+        $app->register(new TwigExtension(), array(
+            'twig.templates'    => array('hello' => 'Hello {{ name }}!'),
+            'twig.class_path'   => __DIR__.'/../../../../vendor/twig/lib',
+        ));
+
+        $app->get('/hello/{name}', function($name) use ($app) {
+            return $app['twig']->render('hello', array('name' => $name));
+        });
+
+        $request = Request::create('/hello/john');
+        $response = $app->handle($request);
+        $this->assertEquals('Hello john!', $response->getContent());
+    }
+}
