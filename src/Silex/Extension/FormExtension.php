fbfa8e499047b84685c1bd554ee66ce1c417fcb0
Fabien Potencier <fabien.potencier@gmail.com>
2011-04-23 09:29:32 +0200

    updated FormExtension to work with the latest Symfony version

--- src/Silex/Extension/FormExtension.php
+++ src/Silex/Extension/FormExtension.php
@@ -14,35 +14,38 @@ namespace Silex\Extension;
 use Silex\Application;
 use Silex\ExtensionInterface;
 use Symfony\Component\HttpFoundation\File\TemporaryStorage;
-use Symfony\Component\Form\CsrfProvider\DefaultCsrfProvider;
-use Symfony\Component\Form\Type\Loader\DefaultTypeLoader;
+use Symfony\Component\Form\Extension\Csrf\CsrfProvider\DefaultCsrfProvider;
 use Symfony\Component\Form\FormFactory;
+use Symfony\Component\Form\Extension\Core\CoreExtension;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
 
 class FormExtension implements ExtensionInterface
 {
     public function register(Application $app)
     {
-        $app['form.csrf.secret'] = '12345';
-        $app['form.storage.secret'] = 'abcdef';
+        $app['form.csrf_secret'] = '12345';
+        $app['form.storage_secret'] = 'abcdef';
 
         $app['form.factory'] = $app->share(function () use ($app) {
-            return new FormFactory($app['form.type_loader']);
-        });
+            $extensions = array(
+                new CoreExtension($app['form.storage']),
+                new CsrfExtension($app['form.csrf_provider']),
+            );
 
-        $app['form.type_loader'] = $app->share(function () use ($app) {
-            if (!isset($app['validator'])) {
-                throw new \RuntimeException(sprintf('The FormExtension needs the ValidationExtension to be registered.'));
+            if (isset($app['validator'])) {
+                $extensions[] = new ValidatorExtension($app['validator']);
             }
 
-            return new DefaultTypeLoader($app['validator'], $app['form.csrf.provider'], $app['form.storage']);
+            return new FormFactory($extensions);
         });
 
-        $app['form.csrf.provider'] = $app->share(function () use ($app) {
-            return new DefaultCsrfProvider($app['form.csrf.secret']);
+        $app['form.csrf_provider'] = $app->share(function () use ($app) {
+            return new DefaultCsrfProvider($app['form.csrf_secret']);
         });
 
         $app['form.storage'] = $app->share(function () use ($app) {
-            return new TemporaryStorage($app['form.storage.secret']);
+            return new TemporaryStorage($app['form.storage_secret']);
         });
 
         if (isset($app['form.class_path'])) {
