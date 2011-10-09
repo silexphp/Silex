8bdc0a8ba6ce3b4a868916a1cefb041d1542279c
Fabien Potencier <fabien.potencier@gmail.com>
2011-10-09 11:28:25 +0200

    added a script to update git submodules

---
+++ update_vendors.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+COMPONENTS='BrowserKit CssSelector EventDispatcher HttpFoundation Process ClassLoader DomCrawler Finder HttpKernel Routing'
+
+cd vendor/Symfony/Component
+for COMPONENT in $COMPONENTS
+do
+    cd $COMPONENT && git fetch origin && git reset --hard origin/2.0 && cd ..
+done
+cd ../../..
+
+cd vendor/doctrine-common
+git fetch origin && git reset --hard origin/2.1.x
+cd ../..
+
+cd vendor/doctrine-dbal
+git fetch origin && git reset --hard origin/2.1.x
+cd ../..
+
+cd vendor/monolog
+git fetch origin && git reset --hard 1.0.1
+cd ../..
+
+cd vendor/pimple
+git fetch origin && git reset --hard origin/master
+cd ../..
+
+cd vendor/twig
+git fetch origin && git reset --hard v1.3.0
+cd ../..
