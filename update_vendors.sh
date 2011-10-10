b832c3467b92b41cc34aa893deb3a1405295ccd6
Fabien Potencier <fabien.potencier@gmail.com>
2011-10-10 13:34:26 +0200

    switched vendors to use Symfony 2.1

--- update_vendors.sh
+++ update_vendors.sh
@@ -5,7 +5,7 @@ COMPONENTS='BrowserKit CssSelector EventDispatcher HttpFoundation Process ClassL
 cd vendor/Symfony/Component
 for COMPONENT in $COMPONENTS
 do
-    cd $COMPONENT && git fetch origin && git reset --hard origin/2.0 && cd ..
+    cd $COMPONENT && git fetch origin && git reset --hard origin/master && cd ..
 done
 cd ../../..
 
