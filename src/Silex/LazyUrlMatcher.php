82685535c2be4f9c1e51b234044ebceb0d321970
Igor Wiedler <igor@wiedler.ch>
2011-10-27 19:35:36 +0200

    Create default set of listeners late
    Right now the following subscribers are created together with the
    dispatcher:
    
    * app
    * exception_handler
    * ResponseListener
    * RouterListener
    
    With the exception_handler in particular this is an issue because you
    can no longer change it after the dispatcher has been created. And it
    gets created when you call before(), after() or error(). It means that
    you cannot unset it for tests.
    
    The RouterListener has a similar issue because you can no longer change
    the UrlMatcher it uses. And you cannot move its creation into a request
    listener, because request listeners cannot add other request listeners
    (the new ones won't get executed anymore at that point).
    
    This commit ensures that those listeners will be created a lot later,
    allowing their dependencies to be modified before runtime.

---
+++ src/Silex/LazyUrlMatcher.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Silex framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Silex;
+
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+
+/**
+ * Implements the RedirectableUrlMatcherInterface for Silex.
+ *
+ * @author Igor Wiedler <igor@wiedler.ch>
+ */
+class LazyUrlMatcher implements UrlMatcherInterface
+{
+    private $factory;
+    private $urlMatcher;
+
+    public function __construct(\Closure $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function getUrlMatcher()
+    {
+        $urlMatcher = call_user_func($this->factory);
+        if (!$urlMatcher instanceof UrlMatcherInterface) {
+            throw new \LogicException("Factory supplied to LazyUrlMatcher must return implementation of UrlMatcherInterface.");
+        }
+        return $urlMatcher;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match($pathinfo)
+    {
+        return $this->getUrlMatcher()->match($pathinfo);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setContext(RequestContext $context)
+    {
+        $this->getUrlMatcher()->setContext($context);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getContext()
+    {
+        return $this->getUrlMatcher()->getContext();
+    }
+}
