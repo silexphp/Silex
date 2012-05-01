6ad4c16aee31936e8433a191623311aa57e4fa2c
Fabien Potencier <fabien.potencier@gmail.com>
2012-05-02 00:11:20 +0200

    fixed phar creation

--- src/Silex/Compiler.php
+++ src/Silex/Compiler.php
@@ -70,9 +70,9 @@ class Compiler
 
         $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../LICENSE'), false);
         $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/autoload.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/ClassLoader.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/autoload_namespaces.php'));
-        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/autoload_classmap.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/ClassLoader.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_namespaces.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_classmap.php'));
 
         // Stubs
         $phar->setStub($this->getStub());
