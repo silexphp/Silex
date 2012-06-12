4b70fb1a9b498228146ba0f363209b5829f10ca7
Fabien Potencier <fabien.potencier@gmail.com>
2012-06-12 09:20:09 +0200

    fixed session service provider for functional tests

--- src/Silex/Provider/SessionServiceProvider.php
+++ src/Silex/Provider/SessionServiceProvider.php
@@ -16,8 +16,13 @@ use Silex\ServiceProviderInterface;
 
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\FileSessionHandler;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 /**
  * Symfony HttpFoundation component Provider for sessions.
@@ -32,7 +37,19 @@ class SessionServiceProvider implements ServiceProviderInterface
     {
         $this->app = $app;
 
+        if (!isset($app['session.test'])) {
+            $app['session.test'] = false;
+        }
+
         $app['session'] = $app->share(function () use ($app) {
+            if (!isset($app['session.storage'])) {
+                if ($app['session.test']) {
+                    $app['session.storage'] = $app['session.storage.test'];
+                } else {
+                    $app['session.storage'] = $app['session.storage.native'];
+                }
+            }
+
             return new Session($app['session.storage']);
         });
 
@@ -42,11 +59,15 @@ class SessionServiceProvider implements ServiceProviderInterface
             );
         });
 
-        $app['session.storage'] = $app->share(function () use ($app) {
+        $app['session.storage.native'] = $app->share(function () use ($app) {
             return new NativeSessionStorage(
                 $app['session.storage.options'],
                 $app['session.storage.handler']
             );
+        });
+
+        $app['session.storage.test'] = $app->share(function() {
+            return new MockFileSessionStorage();
         });
 
         if (!isset($app['session.storage.options'])) {
@@ -58,7 +79,7 @@ class SessionServiceProvider implements ServiceProviderInterface
         }
     }
 
-    public function onKernelRequest($event)
+    public function onEarlyKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
         $request->setSession($this->app['session']);
@@ -69,8 +90,49 @@ class SessionServiceProvider implements ServiceProviderInterface
         }
     }
 
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        // bootstrap the session
+        if (!isset($this->app['session'])) {
+            return;
+        }
+
+        $session = $this->app['session'];
+        $cookies = $event->getRequest()->cookies;
+
+        if ($cookies->has($session->getName())) {
+            $session->setId($cookies->get($session->getName()));
+        } else {
+            $session->migrate(false);
+        }
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        if ($session = $event->getRequest()->getSession()) {
+            $session->save();
+
+            $params = session_get_cookie_params();
+
+            $event->getResponse()->headers->setCookie(new Cookie($session->getName(), $session->getId(), 0 === $params['lifetime'] ? 0 : time() + $params['lifetime'], $params['path'], $params['domain'], $params['secure'], $params['httponly']));
+        }
+    }
+
     public function boot(Application $app)
     {
-        $app['dispatcher']->addListener(KernelEvents::REQUEST, array($this, 'onKernelRequest'), 128);
+        $app['dispatcher']->addListener(KernelEvents::REQUEST, array($this, 'onEarlyKernelRequest'), 128);
+
+        if ($app['session.test']) {
+            $app['dispatcher']->addListener(KernelEvents::REQUEST, array($this, 'onKernelRequest'), 192);
+            $app['dispatcher']->addListener(KernelEvents::RESPONSE, array($this, 'onKernelResponse'), -128);
+        }
     }
 }
