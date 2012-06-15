788d41c00f6ac37c76c6359e6831248eb85ac95b
Fabien Potencier <fabien.potencier@gmail.com>
2012-06-15 12:08:36 +0200

    moved the checks for *.class_path parameters to the boot method as people might have defined the parameter after the register method is called

--- src/Silex/Provider/MonologServiceProvider.php
+++ src/Silex/Provider/MonologServiceProvider.php
@@ -56,14 +56,14 @@ class MonologServiceProvider implements ServiceProviderInterface
                 return Logger::DEBUG;
             };
         }
-
-        if (isset($app['monolog.class_path'])) {
-            throw new \RuntimeException('You have provided the monolog.class_path parameter. The autoloader has been removed from Silex. It is recommended that you use Composer to manage your dependencies and handle your autoloading. If you are already using Composer, you can remove the parameter. See http://getcomposer.org for more information.');
-        }
     }
 
     public function boot(Application $app)
     {
+        if (isset($app['monolog.class_path'])) {
+            throw new \RuntimeException('You have provided the monolog.class_path parameter. The autoloader has been removed from Silex. It is recommended that you use Composer to manage your dependencies and handle your autoloading. If you are already using Composer, you can remove the parameter. See http://getcomposer.org for more information.');
+        }
+
         $app->before(function (Request $request) use ($app) {
             $app['monolog']->addInfo('> '.$request->getMethod().' '.$request->getRequestUri());
         });
