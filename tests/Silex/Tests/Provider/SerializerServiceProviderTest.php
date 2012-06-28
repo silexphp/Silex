03bccc83556cad3d10bcca7bbe551177a3eaee96
Marijn Huizendveld <marijn.huizendveld@gmail.com>
2012-06-28 16:09:18 +0200

    Add a `SerializerServiceProvider`.
    Usage
    -----
    
    ```php
    <?php
    
    use Silex\Application;
    use Silex\Provider\SerializerServiceProvider;
    use Symfony\Component\HttpFoundation\Response;
    
    $app = new Application();
    
    $app->register(new SerializerServiceProvider);
    
    // only accept content types supported by the serializer via the assert method.
    $app->get("/pages/{id}.{_format}", function ($id) use ($app) {
        // assume a page_repository service exists that returns Page objects. The
        // object returned has getters and setters exposing the state.
        $page = $app['page_repository']->find($id);
        $format = $app['request']->getFormat();
    
        if (!$page instanceof Page) {
            $this->abort("No page found for id: $id");
        }
    
        return new Response($app['serializer']->serialize($page, $format), 200, array(
            "Content-Type" => $app['request']->getMimeType($format)
        ));
    })->assert("_format", "xml|json")
      ->assert("id", "\d+");
    ```

---
+++ tests/Silex/Tests/Provider/SerializerServiceProviderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Silex framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Silex\Tests\Provider;
+
+use Silex\Application;
+use Silex\Provider\SerializerServiceProvider;
+
+/**
+ * SerializerServiceProvider test cases.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ */
+class SerializerServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRegister()
+    {
+        $app = new Application();
+
+        $app->register(new SerializerServiceProvider);
+
+        $this->assertInstanceOf("Symfony\Component\Serializer\Serializer", $app['serializer']);
+        $this->assertTrue($app['serializer']->supportsEncoding('xml'));
+        $this->assertTrue($app['serializer']->supportsEncoding('json'));
+        $this->assertTrue($app['serializer']->supportsDecoding('xml'));
+        $this->assertTrue($app['serializer']->supportsDecoding('json'));
+    }
+}
