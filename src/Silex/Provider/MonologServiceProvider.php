1cc04529e7103f941476b225bb588ecdbf4c31ad
till <till@php.net>
2013-02-04 16:44:56 +0100

    Add a stacktrace to the log messagen when `$app['debug'] = true;`

--- src/Silex/Provider/MonologServiceProvider.php
+++ src/Silex/Provider/MonologServiceProvider.php
@@ -72,6 +72,9 @@ class MonologServiceProvider implements ServiceProviderInterface
 
         $app->error(function (\Exception $e) use ($app) {
             $message = sprintf('%s: %s (uncaught exception) at %s line %s', get_class($e), $e->getMessage(), $e->getFile(), $e->getLine());
+            if (true === $app['debug']) {
+                $message .= sprintf('Stacktrace: %s', $e->getTraceAsString());
+            }
             if ($e instanceof HttpExceptionInterface && $e->getStatusCode() < 500) {
                 $app['monolog']->addError($message);
             } else {
