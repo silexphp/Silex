aa75ec9859d29d130a6103cee7326749c6a11b7b
Igor Wiedler <igor@wiedler.ch>
2013-02-09 18:27:22 +0100

    Remove intl code from tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,3 @@
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('Silex\Tests', __DIR__);
-
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->add('IntlDateFormatter', __DIR__.'/../vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs');
-    $loader->add('Collator', __DIR__.'/../vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs');
-    $loader->add('Locale', __DIR__.'/../vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs');
-    $loader->add('NumberFormatter', __DIR__.'/../vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs');
-}
