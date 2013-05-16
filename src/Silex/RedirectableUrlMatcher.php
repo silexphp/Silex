3d1a868c895901cc082804f55944cc13e9ba9d79
luisgonzalez <jlglez@gmail.com>
2013-05-16 17:52:41 -0400

    add '_route' key to RedirectableUrlMatcher::redirect
    When there is a logger, the Symfony\Compoment\HttpKernel\EventListener\RouterListener class expects the '_route' key to exist in order to log the matched route and issues a PHP Notice

--- src/Silex/RedirectableUrlMatcher.php
+++ src/Silex/RedirectableUrlMatcher.php
@@ -54,6 +54,7 @@ class RedirectableUrlMatcher extends BaseRedirectableUrlMatcher
 
         return array(
             '_controller' => function ($url) { return new RedirectResponse($url, 301); },
+            '_route' => null,
             'url' => $url,
         );
     }
