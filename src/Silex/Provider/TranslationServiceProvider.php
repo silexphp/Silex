92d4f0f1b2be97f1f0d8740fd377a95849f4cdba
Daniel Tschinder <code@tschinder.de>
2013-09-16 09:53:36 +0200

    Remove usage of deprecated function setFallbackLocale

--- src/Silex/Provider/TranslationServiceProvider.php
+++ src/Silex/Provider/TranslationServiceProvider.php
@@ -31,7 +31,7 @@ class TranslationServiceProvider implements ServiceProviderInterface
         $app['translator'] = $app->share(function ($app) {
             $translator = new Translator($app['locale'], $app['translator.message_selector']);
 
-            $translator->setFallbackLocale($app['locale_fallback']);
+            $translator->setFallbackLocales($app['locale_fallbacks']);
 
             $translator->addLoader('array', new ArrayLoader());
             $translator->addLoader('xliff', new XliffFileLoader());
@@ -51,7 +51,9 @@ class TranslationServiceProvider implements ServiceProviderInterface
 
         $app['translator.domains'] = array();
 
-        $app['locale_fallback'] = 'en';
+        $app['locale_fallbacks'] = array(
+            'en'
+        );
     }
 
     public function boot(Application $app)
