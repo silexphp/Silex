599cd1e256e577a97e827ccf91ca5280d2f727d4
Kilte <nwotnbm@gmail.com>
2014-01-18 17:35:49 +0300

    Remove backslashes

--- src/Silex/ControllerCollection.php
+++ src/Silex/ControllerCollection.php
@@ -24,14 +24,14 @@ use Symfony\Component\HttpFoundation\Request;
  * __call() forwards method-calls to Route, but returns instance of ControllerCollection
  * listing Route's methods below, so that IDEs know they are valid
  *
- * @method \Silex\ControllerCollection assert(\string $variable, \string $regexp)
- * @method \Silex\ControllerCollection value(\string $variable, \mixed $default)
- * @method \Silex\ControllerCollection convert(\string $variable, \mixed $callback)
- * @method \Silex\ControllerCollection method(\string $method)
+ * @method \Silex\ControllerCollection assert(string $variable, string $regexp)
+ * @method \Silex\ControllerCollection value(string $variable, mixed $default)
+ * @method \Silex\ControllerCollection convert(string $variable, mixed $callback)
+ * @method \Silex\ControllerCollection method(string $method)
  * @method \Silex\ControllerCollection requireHttp()
  * @method \Silex\ControllerCollection requireHttps()
- * @method \Silex\ControllerCollection before(\mixed $callback)
- * @method \Silex\ControllerCollection after(\mixed $callback)
+ * @method \Silex\ControllerCollection before(mixed $callback)
+ * @method \Silex\ControllerCollection after(mixed $callback)
  *
  * @author Igor Wiedler <igor@wiedler.ch>
  * @author Fabien Potencier <fabien@symfony.com>
