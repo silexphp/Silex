cb8ff372b7801fb6beb1cb3ee82739f816fd177b
1emming <megatron001@gmail.com>
2014-06-27 11:11:58 +0200

    remove not needed checks

--- src/Silex/Provider/RememberMeServiceProvider.php
+++ src/Silex/Provider/RememberMeServiceProvider.php
@@ -75,7 +75,7 @@ class RememberMeServiceProvider implements ServiceProviderInterface, EventListen
                     'remember_me_parameter' => '_remember_me',
                 ), $options);
 
-                return new TokenBasedRememberMeServices(array($app['security.user_provider.'.$providerKey]), $options['key'], $providerKey, $options, isset($app['logger']) ? $app['logger'] : null);
+                return new TokenBasedRememberMeServices(array($app['security.user_provider.'.$providerKey]), $options['key'], $providerKey, $options, $app['logger']);
             };
         });
 
@@ -85,7 +85,7 @@ class RememberMeServiceProvider implements ServiceProviderInterface, EventListen
                     $app['security'],
                     $app['security.remember_me.service.'.$providerKey],
                     $app['security.authentication_manager'],
-                    isset($app['logger']) ? $app['logger'] : null
+                    $app['logger']
                 );
 
                 return $listener;
