eae3f7a82815c0ed22664e98ff273a3c6acc7eae
Kevin Postma <kevinpostma@live.nl>
2014-10-06 16:53:52 +0200

    Added testcase for checking if event has been fired after login with rememberMe authentication.

--- tests/Silex/Tests/Provider/RememberMeServiceProviderTest.php
+++ tests/Silex/Tests/Provider/RememberMeServiceProviderTest.php
@@ -28,20 +28,30 @@ class RememberMeServiceProviderTest extends WebTestCase
     public function testRememberMeAuthentication()
     {
         $app = $this->createApplication();
+		
+		$event = false;
+		$app->on(\Symfony\Component\Security\Http\SecurityEvents::INTERACTIVE_LOGIN, function($event) use ($app, &$event) {
+			$event = true;
+		});
 
         $client = new Client($app);
 
         $client->request('get', '/');
+		$this->assertFalse($event, 'The interactive login has not been triggered yet');
         $client->request('post', '/login_check', array('_username' => 'fabien', '_password' => 'foo', '_remember_me' => 'true'));
         $client->followRedirect();
         $this->assertEquals('AUTHENTICATED_FULLY', $client->getResponse()->getContent());
+		$this->assertTrue($event, 'The interactive login has been triggered');
 
         $this->assertNotNull($client->getCookiejar()->get('REMEMBERME'), 'The REMEMBERME cookie is set');
+		$event = false;
+		$this->assertFalse($event, 'The interactive login has not been triggered yet');
 
         $client->getCookiejar()->expire('MOCKSESSID');
 
         $client->request('get', '/');
         $this->assertEquals('AUTHENTICATED_REMEMBERED', $client->getResponse()->getContent());
+		$this->assertTrue($event, 'The interactive login has been triggered');
 
         $client->request('get', '/logout');
         $client->followRedirect();
