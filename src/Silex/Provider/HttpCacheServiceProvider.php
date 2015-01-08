b92e851e3f01087828a8f5af76a7d04d3c51dbdb
Fabien Potencier <fabien.potencier@gmail.com>
2015-01-08 10:04:30 +0100

    fixed Symfony Http Kernel Cache deprecations

--- src/Silex/Provider/HttpCacheServiceProvider.php
+++ src/Silex/Provider/HttpCacheServiceProvider.php
@@ -17,6 +17,7 @@ use Silex\HttpCache;
 use Symfony\Component\HttpKernel\HttpCache\Esi;
 use Symfony\Component\HttpKernel\HttpCache\Store;
 use Symfony\Component\HttpKernel\EventListener\EsiListener;
+use Symfony\Component\HttpKernel\EventListener\SurrogateListener;
 
 /**
  * Symfony HttpKernel component Provider for HTTP cache.
@@ -46,6 +47,10 @@ class HttpCacheServiceProvider implements ServiceProviderInterface
         });
 
         $app['http_cache.esi_listener'] = $app->share(function ($app) {
+            if (class_exists('Symfony\Component\HttpKernel\EventListener\SurrogateListener')) {
+                return new SurrogateListener($app['http_cache.esi']);
+            }
+
             return new EsiListener($app['http_cache.esi']);
         });
 
