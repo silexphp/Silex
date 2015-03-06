45dc2bcc469414994504040c64f1328fa276db3f
Jim <jimdoescode@users.noreply.github.com>
2015-03-06 11:56:04 -0500

    Add options request capability to controller collection

--- src/Silex/ControllerCollection.php
+++ src/Silex/ControllerCollection.php
@@ -135,6 +135,19 @@ class ControllerCollection
     {
         return $this->match($pattern, $to)->method('DELETE');
     }
+    
+    /**
+     * Maps an OPTIONS request to a callable.
+     *
+     * @param string $pattern Matched route pattern
+     * @param mixed  $to      Callback that returns the response when matched
+     *
+     * @return Controller
+     */
+    public function options($pattern, $to = null)
+    {
+        return $this->match($pattern, $to)->method('OPTIONS');
+    }
 
     /**
      * Maps a PATCH request to a callable.
