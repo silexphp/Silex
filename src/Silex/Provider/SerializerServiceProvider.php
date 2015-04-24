85558b76e9de6d33651368f1b1311f2bed415f6f
Possum <possumfromspace@gmail.com>
2015-04-24 14:55:16 +0200

    Clean ups on master

--- src/Silex/Provider/SerializerServiceProvider.php
+++ src/Silex/Provider/SerializerServiceProvider.php
@@ -28,12 +28,12 @@ use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 class SerializerServiceProvider implements ServiceProviderInterface
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      *
      * This method registers a serializer service. {@link http://api.symfony.com/master/Symfony/Component/Serializer/Serializer.html
      * The service is provided by the Symfony Serializer component}.
      *
-     * @param Pimple $app
+     * @param Container $app
      */
     public function register(Container $app)
     {
