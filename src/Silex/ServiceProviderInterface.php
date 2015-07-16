4b3028e3ec4ab2055f265f54ed912e00a1b98076
Possum <possumfromspace@gmail.com>
2015-07-16 12:55:57 +0200

    Fix PHPDocs

--- src/Silex/ServiceProviderInterface.php
+++ src/Silex/ServiceProviderInterface.php
@@ -23,6 +23,8 @@ interface ServiceProviderInterface
      *
      * This method should only be used to configure services and parameters.
      * It should not get services.
+     *
+     * @param Application $app
      */
     public function register(Application $app);
 
@@ -32,6 +34,8 @@ interface ServiceProviderInterface
      * This method is called after all services are registered
      * and should be used for "dynamic" configuration (whenever
      * a service must be requested).
+     *
+     * @param Application $app
      */
     public function boot(Application $app);
 }
