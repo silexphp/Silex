374c7e04040a6f781c90f7d746726a5daa78e783
Fabien Potencier <fabien.potencier@gmail.com>
2016-01-06 15:59:35 +0100

    prepared the 1.3.5 release

--- src/Silex/Application.php
+++ src/Silex/Application.php
@@ -44,7 +44,7 @@ use Silex\EventListener\StringToResponseListener;
  */
 class Application extends \Pimple implements HttpKernelInterface, TerminableInterface
 {
-    const VERSION = '1.3.5-DEV';
+    const VERSION = '1.3.5';
 
     const EARLY_EVENT = 512;
     const LATE_EVENT = -512;
