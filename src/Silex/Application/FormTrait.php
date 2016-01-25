c8e61fc212267d29fc0b98063a027285cff16112
quazardous <berliozdavid@gmail.com>
2016-01-25 13:25:53 +0100

    add a namedForm() method in FormTrait

--- src/Silex/Application/FormTrait.php
+++ src/Silex/Application/FormTrait.php
@@ -17,6 +17,7 @@ use Symfony\Component\Form\FormBuilder;
  * Form trait.
  *
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author David Berlioz <berliozdavid@gmail.com>
  */
 trait FormTrait
 {
@@ -38,4 +39,25 @@ trait FormTrait
 
         return $this['form.factory']->createBuilder($type, $data, $options);
     }
+
+    /**
+     * Creates and returns a named form builder instance.
+     *
+     * @param string                   $name
+     * @param mixed                    $data    The initial data for the form
+     * @param array                    $options Options for the form
+     * @param string|FormTypeInterface $type    Type of the form
+     *
+     * @return \Symfony\Component\Form\FormBuilder
+     */
+    public function namedForm($name, $data = null, array $options = array(), $type = null)
+    {
+        if (null === $type) {
+            // BC with Symfony < 2.8
+            $type = class_exists('Symfony\Component\Form\Extension\Core\Type\RangeType') ? 'Symfony\Component\Form\Extension\Core\Type\FormType' : 'form';
+        }
+
+        return $this['form.factory']->createNamedBuilder($name, $type, $data, $options);
+    }
+
 }
