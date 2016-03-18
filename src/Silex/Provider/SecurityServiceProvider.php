5a9516ed513689c6f6e480f5d606ad31b59a27e9
Georald Camposano <georald@pgiauto.com>
2016-03-18 12:41:40 -0700

    Added test for access rule path array

--- src/Silex/Provider/SecurityServiceProvider.php
+++ src/Silex/Provider/SecurityServiceProvider.php
@@ -340,7 +340,7 @@ class SecurityServiceProvider implements ServiceProviderInterface, EventListener
                         'host' => null,
                         'methods' => null,
                         'ips' => null,
-                        'attributes' => null,
+                        'attributes' => array(),
                         'schemes' => null,
                     ];
                     $rule[0] = new RequestMatcher($rule[0]['path'], $rule[0]['host'], $rule[0]['methods'], $rule[0]['ips'], $rule[0]['attributes'], $rule[0]['schemes']);
