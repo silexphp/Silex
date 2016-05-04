cecdb85e7ad73432f8318e9d3f545304f04e11a2
RJ Garcia <rj@bighead.net>
2016-05-04 10:49:04 -0700

    Fixed Session Service Provider registry
    The service provider was registering the wrong parameters
    to the session listener instead of the actual session class
    
    Signed-off-by: RJ Garcia <rj@bighead.net>

--- src/Silex/Provider/SessionServiceProvider.php
+++ src/Silex/Provider/SessionServiceProvider.php
@@ -38,7 +38,7 @@ class SessionServiceProvider implements ServiceProviderInterface, EventListenerP
         $app['session.test'] = false;
 
         $app['session'] = function ($app) {
-            return new Session($app['session.storage']);
+            return new Session($app['session.storage'], $app['session.attribute_bag'], $app['session.flash_bag']);
         };
 
         $app['session.storage'] = function ($app) {
@@ -61,7 +61,7 @@ class SessionServiceProvider implements ServiceProviderInterface, EventListenerP
         };
 
         $app['session.listener'] = function ($app) {
-            return new SessionListener($app, $app['session.attribute_bag'], $app['session.flash_bag']);
+            return new SessionListener($app);
         };
 
         $app['session.storage.test'] = function () {
