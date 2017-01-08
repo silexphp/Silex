01f90256c7bb2086025959f85b023feecc3cf524
HeahDude <heah@heahprod.com>
2017-01-08 18:28:27 +0100

    Added the FormRegistry as a service to enable the extension point

--- src/Silex/Provider/FormServiceProvider.php
+++ src/Silex/Provider/FormServiceProvider.php
@@ -16,7 +16,8 @@ use Pimple\ServiceProviderInterface;
 use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
 use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 use Symfony\Component\Form\Extension\Validator\ValidatorExtension as FormValidatorExtension;
-use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\Form\FormRegistry;
 use Symfony\Component\Form\ResolvedFormTypeFactory;
 
 /**
@@ -74,11 +75,11 @@ class FormServiceProvider implements ServiceProviderInterface
         };
 
         $app['form.factory'] = function ($app) {
-            return Forms::createFormFactoryBuilder()
-                ->addExtensions($app['form.extensions'])
-                ->setResolvedTypeFactory($app['form.resolved_type_factory'])
-                ->getFormFactory()
-            ;
+            return new FormFactory($app['form.registry'], $app['form.resolved_type_factory']);
+        };
+
+        $app['form.registry'] = function ($app) {
+            return new FormRegistry($app['form.extensions'], $app['form.resolved_type_factory']);
         };
 
         $app['form.resolved_type_factory'] = function ($app) {
