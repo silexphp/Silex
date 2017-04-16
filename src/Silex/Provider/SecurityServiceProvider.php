697f12180d8d7e8004a4af1afafc6ee60c3659e1
Vladimir Zapletal <vlad.zapletal@gmail.com>
2017-04-16 10:55:40 +0200

    Applied codding standards

--- src/Silex/Provider/SecurityServiceProvider.php
+++ src/Silex/Provider/SecurityServiceProvider.php
@@ -219,10 +219,10 @@ class SecurityServiceProvider implements ServiceProviderInterface, EventListener
                 $methods = isset($firewall['methods']) ? $firewall['methods'] : null;
 
                 unset(
-                    $firewall['pattern'], 
-                    $firewall['users'], 
-                    $firewall['security'], 
-                    $firewall['stateless'], 
+                    $firewall['pattern'],
+                    $firewall['users'],
+                    $firewall['security'],
+                    $firewall['stateless'],
                     $firewall['context'],
                     $firewall['methods'],
                     $firewall['hosts']
@@ -307,8 +307,8 @@ class SecurityServiceProvider implements ServiceProviderInterface, EventListener
                 }
 
                 $configs[$name] = array(
-                    'pattern' => $pattern, 
-                    'listeners' => $listeners, 
+                    'pattern' => $pattern,
+                    'listeners' => $listeners,
                     'protected' => $protected,
                     'methods' => $methods,
                     'hosts' => $hosts,
@@ -371,14 +371,14 @@ class SecurityServiceProvider implements ServiceProviderInterface, EventListener
                 if (is_string($rule[0])) {
                     $rule[0] = new RequestMatcher($rule[0]);
                 } elseif (is_array($rule[0])) {
-                    $rule[0] += [
+                    $rule[0] += array(
                         'path' => null,
                         'host' => null,
                         'methods' => null,
                         'ips' => null,
                         'attributes' => array(),
                         'schemes' => null,
-                    ];
+                    );
                     $rule[0] = new RequestMatcher($rule[0]['path'], $rule[0]['host'], $rule[0]['methods'], $rule[0]['ips'], $rule[0]['attributes'], $rule[0]['schemes']);
                 }
                 $map->add($rule[0], (array) $rule[1], isset($rule[2]) ? $rule[2] : null);
