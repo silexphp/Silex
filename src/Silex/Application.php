af928237db79fa3fd0e624d80d863bc779231611
Fabien Potencier <fabien.potencier@gmail.com>
2017-07-23 09:42:19 +0200

    bumped version to 2.2.1-DEV

--- src/Silex/Application.php
+++ src/Silex/Application.php
@@ -41,7 +41,7 @@ use Silex\Provider\HttpKernelServiceProvider;
  */
 class Application extends Container implements HttpKernelInterface, TerminableInterface
 {
-    const VERSION = '2.2.0-DEV';
+    const VERSION = '2.2.1-DEV';
 
     const EARLY_EVENT = 512;
     const LATE_EVENT = -512;
